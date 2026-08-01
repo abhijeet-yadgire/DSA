1d8abe06e432cbbfad74dd110b12f3fda76bee09
Abhijeet Yadgire <abhijeetgithub7@gmail.com>
2026-08-01 22:16:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (85.12%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n);
+        
+        for (int i = 0; i < n; ++i) {
+            dp[i] = nums[i];
+        }
+        
+        for (int len = 2; len <= n; ++len) {
+            for (int i = 0; i <= n - len; ++i) {
+                int j = i + len - 1;
+                dp[i] = std::max(nums[i] - dp[i + 1], nums[j] - dp[i]);
+            }
+        }
+        
+        return dp[0] >= 0;
+    }
+};

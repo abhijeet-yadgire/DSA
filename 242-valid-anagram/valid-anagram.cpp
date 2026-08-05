57a7304747f9ea9d12669167050ccc701d4dfa19
Abhijeet Yadgire <abhijeetgithub7@gmail.com>
2026-08-05 15:13:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (50.07%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector <int> count(26,0);
+
+        for(char i : s){
+            count[i-'a']++;
+        }
+
+        for(char i : t){
+            count[i-'a']--;
+        }
+
+        for(int i : count){
+            if(i != 0)
+                return false;
+        }
+
+        return true;
+    }
+};

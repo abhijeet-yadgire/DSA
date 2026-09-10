dc45b4f8c0a2dfb033defac8bff00663a75210c1
Abhijeet Yadgire <abhijeetgithub7@gmail.com>
2026-09-10 13:20:18 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (52.47%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,11 +4,12 @@ public:
         int currSum = 0;
         int maxSum = INT_MIN;
 
-        for(int i = 0; i < nums.size(); i++){
+        for(int i = 0; i < nums.size(); i++){       
             currSum += nums[i];
             maxSum = max(currSum, maxSum);
-
-            if(currSum < 0) currSum = 0;
+            if(currSum < 0){
+                currSum = 0;
+            }          
         }
 
         return maxSum;
